80ca52f01c7edc4a413219f96316a600f409cfe7
yonatandiga12 <digayona@post.bgu.ac.il>
2021-05-25 11:23:01 +0300

    g

--- dnd/src/Players/Mage.java
+++ dnd/src/Players/Mage.java
@@ -6,18 +6,19 @@ public class Mage extends Player {
     public int currentMana;
     public int manaCost;
     public int spellPower;
-    public int maxHits;
-    public int abilityrange;
+    public int hitsCount ;
+    public int abilityRange;
 
 
-    public Mage(String name, int health, int attack, int defense, int manaPool, int manaCost, int spellPower, int maxHits, int abilityRange){
+    public Mage(String name, int health, int attack, int defense, int manaPool, int manaCost, int spellPower, int hitsCount , int abilityRange){
         super(name, health, attack, defense);
         this.manaPool = manaPool;
         this.currentMana = manaPool / 4;
         this.manaCost = manaCost;
         this.spellPower = spellPower;
-        this.maxHits = maxHits;
-        this.abilityrange = abilityRange;
+        this.hitsCount = hitsCount ;
+        this.abilityRange = abilityRange;
+        this.ability = "Blizzard";
     }
 
 
@@ -30,13 +31,14 @@ public class Mage extends Player {
 
     @Override
     public void castAbility() {
-        //current mana ← current mana − cost
-        //hits ← 0
-        //while (hits < hits count) ∧ (∃ living enemy s.t. range(enemy, player) < ability range) do
-        //    - Select random enemy within ability range.
-        //        - Deal damage (reduce health value) to the chosen enemy for an amount equal to spell power
-        //(each enemy may try to defend itself).
-        //- hits ← hits + 1
+        currentMana = currentMana - manaCost;
+        int hits = 0;
+        while (hits < hitsCount /* &  range(enemy, player) < abilityRange */ ) {  // ∧ (∃ living enemy s.t. range(enemy, player) < ability range) do
+            //Select random enemy within ability range.
+            //Deal damage (reduce health value) to the chosen enemy for an amount equal to spell power
+            //(each enemy may try to defend itself).
+            hits += 1;
+        }
 
     }
 
