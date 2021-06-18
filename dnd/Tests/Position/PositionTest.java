687495e7c4b690c8faf61db94891b551ff5523c3
yotamdinar <80031155+yotamdinar@users.noreply.github.com>
2021-06-18 16:47:36 +0300

    Position test

--- dnd/Tests/Position/PositionTest.java
+++ dnd/Tests/Position/PositionTest.java
@@ -7,23 +7,23 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class PositionTest {
 
+    Position p1;
+    Position p2;
+    Position p3;
+    char c;
+
     @BeforeEach
     void setUp() {
+        p1 = new Position(2,2);
+        p2 = new Position(2, 2);
+        p3 = new Position(2, 1);
     }
 
-    @Test
-    void getRange() {
-    }
-
-    @Test
-    void getX() {
-    }
-
-    @Test
-    void getY() {
-    }
 
     @Test
     void compareTo() {
+        assertEquals(p1.compareTo(p2), 0);
     }
+
+
 }
