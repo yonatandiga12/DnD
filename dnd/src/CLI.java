b85ed633f51a7d4eee47cd9f94d22b4986dbae45
yotamdinar <80031155+yotamdinar@users.noreply.github.com>
2021-06-18 15:33:18 +0300

    git statusgit statusgit status  :)

--- dnd/src/CLI.java
+++ dnd/src/CLI.java
@@ -1,5 +1,6 @@
 import java.io.File;
 import java.net.URL;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class CLI {
@@ -9,15 +10,17 @@ public class CLI {
     public static void main(String[] args){
 
         //C:\בן גוריון\סמסטר ב\מונחה עצמים\Project3\levels_dirC:\Users\dyota\Desktop\מונחה עצמים\git\dnd\levels_dir
-        //GameManager gameManager = new GameManager( "C:\\Users\\dyota\\Desktop\\מונחה עצמים\\git\\dnd\\levels_dir", 3 ); // insert the location
+        //GameManager gameManager = new GameManager( "C:\Users\dyota\Desktop\OOP\git\dnd\levels_dir", 3 ); // insert the location
         Scanner scan = new Scanner(System.in);
-
-        //String levelsPath = args[0];
+        String levelsPath = "";
+        for (String s : args){
+            levelsPath = levelsPath + s;
+        }
 
         GameManager gameManager = new GameManager();
         int l = scan.nextInt();
-        gameManager.choosePlayer("C:\\בן גוריון\\סמסטר ב\\מונחה עצמים\\Project3\\levels_dir", l);
-        //gameManager.choosePlayer(levelsPath, l);
+        //gameManager.choosePlayer("C:\\בן גוריון\\סמסטר ב\\מונחה עצמים\\Project3\\levels_dir", l);
+        gameManager.choosePlayer(levelsPath, l);
 
         while(gameManager.isGameActive()) {
             String letter = scan.nextLine();
